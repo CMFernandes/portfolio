400a6983fa3188f8542d8ad374b73735d42400d3
cmfernandes <cristianafmfernandes@gmail.com>
2024-03-26 09:50:36 +0000

    create spans for name title to do animation delay

--- index.js
+++ index.js
@@ -13,3 +13,21 @@ window.addEventListener('scroll', toggleHeaderVisibility);
 window.addEventListener('touchmove', toggleHeaderVisibility);
 
 toggleHeaderVisibility()
+
+
+const nameIntro = document.querySelector(".name-intro");
+const letters = nameIntro.textContent.split("");
+
+nameIntro.textContent = "";
+
+letters.forEach(letter => {
+    const span = document.createElement('span');
+    span.textContent = letter;
+    nameIntro.appendChild(span);
+});
+
+const spans = nameIntro.querySelectorAll('span');
+spans.forEach((span, index) => {
+    span.style.animationDelay = `${index * 0.1}s`; 
+});
+
