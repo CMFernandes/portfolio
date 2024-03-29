ddfcdeb0f405f7d029718895687c3c163e2305ac
cmfernandes <cristianafmfernandes@gmail.com>
2024-03-29 18:12:50 +0000

    refactor createspans with anim delay & add theme toggle func

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//make the header visible when leaving intro 
 const header = document.querySelector(".header");
 const intro = document.getElementById("intro");
 
@@ -15,19 +16,51 @@ window.addEventListener('touchmove', toggleHeaderVisibility);
 toggleHeaderVisibility()
 
 
+//create spans with animation delay
+let index = 0;
+
+function createSpansWithAnimationDelay(text,container){
+    const letters = text.split("");
+
+    letters.forEach((letter) => {
+        const span = document.createElement("span");
+        span.textContent = letter;
+        container.appendChild(span);
+        span.style.animationDelay = `${index * 0.1}s`;  
+        index++;
+    });
+};
+
 const nameIntro = document.querySelector(".name-intro");
-const letters = nameIntro.textContent.split("");
+createSpansWithAnimationDelay("Cristiana Fernandes", nameIntro);
 
-nameIntro.textContent = "";
+const introText = document.querySelector(".intro-text");
+createSpansWithAnimationDelay("An aspiring Front-End Developer", introText);
 
-letters.forEach(letter => {
-    const span = document.createElement('span');
-    span.textContent = letter;
-    nameIntro.appendChild(span);
+
+//toggle theme 
+const root = document.documentElement;
+const themeToggles = document.querySelectorAll(".theme-toggle");
+
+
+function toggleTheme(){
+    themeToggles.forEach(button => {
+        if (button.classList.contains("fa-moon")) {
+            toggleClass("fa-moon", "fa-sun", button);
+        } else {
+            toggleClass("fa-sun", "fa-moon", button);
+        }
+    })
+    
+    root.classList.toggle('dark-theme');
+}
+
+themeToggles.forEach(button => {
+    button.addEventListener('click', toggleTheme);
+    button.addEventListener('touchstart', toggleTheme);
 });
 
-const spans = nameIntro.querySelectorAll('span');
-spans.forEach((span, index) => {
-    span.style.animationDelay = `${index * 0.1}s`; 
-});
-
+function toggleClass(remove,add,element){
+    element.classList.remove(remove);
+    element.classList.add(add);
+}
